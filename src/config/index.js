d7af0dff127c4fb99c75c4fd9e87fc4825aeed89
pirabanjan <pirabanjan@gmail.com>
2023-11-19 19:37:38 -0500

    added files

--- src/config/index.js
+++ src/config/index.js
@@ -1,26 +1,4 @@
 const dotEnv = require("dotenv");
-const fs = require('fs');
-
-const loadConfigFromK8S = () => {
-  try {
-    const configMapPath = '/etc/config/shopping-config.json'; // Adjust the path accordingly
-    const configMapData = fs.readFileSync(configMapPath, 'utf8');
-    const configMapJson = JSON.parse(configMapData);
-
-    process.env.PORT = configMapJson.PORT;
-    process.env.MONGODB_URI = configMapJson.MONGODB_URI;
-    process.env.APP_SECRET = configMapJson.APP_SECRET;
-    process.env.BASE_URL = configMapJson.BASE_URL;
-    process.env.EXCHANGE_NAME = configMapJson.EXCHANGE_NAME;
-    process.env.MSG_QUEUE_URL = configMapJson.MSG_QUEUE_URL;
-  } catch (error) {
-    console.error('Error loading configuration from ConfigMap:', error);
-    // Handle the error as needed
-  }
-};
-
-// Load configuration from Kubernetes ConfigMap
-loadConfigFromK8S();
 
 if (process.env.NODE_ENV !== "prod") {
   const configFile = `./.env.${process.env.NODE_ENV}`;
